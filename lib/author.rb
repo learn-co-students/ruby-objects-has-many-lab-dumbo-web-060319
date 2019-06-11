f18d923e9fd3f925e69f328e6264dbdaebcf3e96
Gia Edgington <giaedgington@Erics-MacBook.local>
2019-06-11 10:36:43 -0400

    Done.

---
+++ lib/author.rb
@@ -0,0 +1,31 @@
+require 'pry'
+
+class Author
+    attr_accessor :name, :posts
+
+    def initialize(name)
+        @name = name
+        @posts = []
+    end
+
+    def add_post(post)
+        @posts << post
+        post.author = self
+      end
+    
+    def add_post_by_title(title)
+        post = Post.new(title)
+        @posts << post
+        post.author = self
+    end 
+
+    def self.post_count
+        Post.all.count
+    end
+
+end
+
+#author1 = Author.new("Asimov")
+#author1.add_post("post1")
+
+#binding.pry
